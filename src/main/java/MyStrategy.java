87febc5f21e1f9dd1f67152d07db7c331efadf8a
Liavontsi Brechka <lbrechka@ciena.com>
2016-11-28 22:48:36 -0500

    Null pointer exception handling for skills property (version update 15)

--- src/main/java/MyStrategy.java
+++ src/main/java/MyStrategy.java
@@ -69,6 +69,7 @@ public final class MyStrategy implements Strategy {
             else enemyFaction = Faction.ACADEMY;
             wayPoints = new WayPoints(self, game);
             collisionHandler = new CollisionHandler();
+            skills = new HashSet<>();
         }
     }
 
